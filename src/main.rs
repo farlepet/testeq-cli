e91199c0b4d132303410d0ed4c772f8248a972ca
Peter Farley <pfarley@pfarley.dev>
2026-01-13 11:58:09 -0600

    Add version and help query

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,65 @@ use crate::devices::{dmm, psu};
 mod command;
 mod devices;
 
+fn handle_args(args: &mut Vec<String>) -> Result<bool> {
+    let i = 1;
+
+    while i < args.len() {
+        if !args[i].starts_with('-') {
+            /* End of flags */
+            break;
+        }
+
+        match args[i].as_str() {
+            "-v" | "--version" => {
+                println!("Version: {}", env!("CARGO_PKG_VERSION"));
+                println!("testeq-rs version: {}", testeq_rs::version());
+
+                return Ok(true);
+            }
+            "-h" | "--help" => {
+                print_usage();
+                return Ok(true);
+            }
+            "-V" | "--verbose" => {
+                /* TODO, leaving unimplemented just to be able to keep loop
+                 * without warnings. */
+            }
+            flag => {
+                print_usage();
+                println!();
+                bail!("Unknown option `{flag}`")
+            }
+        }
+
+        args.remove(i);
+    }
+
+    Ok(false)
+}
+
+fn print_usage() {
+    println!("Usage: testeq-cli [flags] <uri> ...");
+    println!();
+    println!("flags:");
+    println!("  -v --version   Print version of testeq-cli");
+    println!("  -h --help      Show this help message");
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let args: Vec<_> = env::args().collect();
+    let mut args: Vec<_> = env::args().collect();
 
     if args.len() < 2 {
-        bail!("Usage: testeq-cli <uri> ...")
+        print_usage();
+        println!();
+        bail!("Missing <uri>");
+    }
+
+    if handle_args(&mut args)? {
+        return Ok(());
     }
 
     let uri = &args[1];
